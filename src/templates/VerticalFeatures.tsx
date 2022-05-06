d8ec09891b5e025d8233d961058bc5c98c99c937
VictorGreco <victorgrecotech@gmail.com>
2022-05-07 00:23:08 +0200

    fixed texts

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -6,26 +6,26 @@ const { about } = AppConfig.menu;
 
 const VerticalFeatures = () => (
   <Section
-    title="We strongly believe..."
-    description="good teams makes great projects succeed."
+    title="We strongly believe in..."
+    description="good teams can make great projects succeed."
     id={about.targetId}
   >
     <VerticalFeatureRow
       title="Transparency"
-      description="We connect all the dots for you so... You can make informed decisions and we can create better solutions for your needs."
+      description="We connect all the dots so... You can make informed decisions and we can create better solutions."
       image="/assets/images/feature.svg"
       imageAlt="First feature alt text"
     />
     <VerticalFeatureRow
-      title="Commitment to Quality"
-      description="Web development is part of our backbone and we know the value of a high quality code, good practices and optimised resources."
+      title="Commitment"
+      description="Web development is part of our backbone. We know the value of a high quality code and optimised resources."
       image="/assets/images/feature2.svg"
       imageAlt="Second feature alt text"
       reverse
     />
     <VerticalFeatureRow
-      title="Innovation &amp; Growth"
-      description="Our team is constantly looking for tech leads on different areas in love with technology and new challenges. We learn from each project while providing value at the same time."
+      title="Innovation"
+      description="Our team is constantly looking for tech leads in love with technology. We learn from each project while providing value."
       image="/assets/images/feature3.svg"
       imageAlt="Third feature alt text"
     />
